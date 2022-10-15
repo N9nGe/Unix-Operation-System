4e35f9567dc4881a7755a36f98722650b87ab6b7
Wang <jiew5@illinois.edu>
2022-10-14 22:54:21 -0500

    Finish PIC without debug and running

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -4,53 +4,132 @@
 
 #include "i8259.h"
 #include "lib.h"
-#include<stdio.h>
+#include<stdio.h>  // TODO
 
 /* Interrupt masks to determine which interrupts are enabled and disabled */
 uint8_t master_mask; /* IRQs 0-7  */
 uint8_t slave_mask;  /* IRQs 8-15 */
-// Can we use it directly?
-spin_lock_t i8259_lock;
 
-/* Initialize the 8259 PIC */
+#define MASTER_8259_DATA MASTER_8259_PORT + 1
+#define SLAVE_8259_DATA  SLAVE_8259_PORT + 1
+/* 
+ * i8259_init
+ *  DESCRIPTION: Initialize the 8259 PIC
+ *  INPUTS: none
+ *  OUTPUTS: none
+ *  RETURN VALUE: none
+ *  SIDE EFFECTS: none
+ */
 // about 14 col
 void i8259_init(void) {
-    unsigned long flags;
-    int eoi; // ??
-    // spin_lock_irqsave(&i8259_lock,flags);
-    outb(0xff,0x21);
-    outb(0xff,0xA1);
-    /*outb_p - this should work on a wide range of PC device */
-    outb_p(ICW1,MASTER_8259_PORT);
-    outb_p(ICW2_MASTER + 0, 0x21);
-    outb_p(ICW3_MASTER,0x21);
+    unsigned long flags;  // EFLAG saving 
+    master_mask = 0xff;
+    slave_mask  = 0xff;
+    // printf("initiliazing the PIC")
+    cli_and_save(flags);
+    outb(master_mask,MASTER_8259_DATA);
+    outb(slave_mask,SLAVE_8259_DATA);
+    // First initilize MASTER pic
+    outb(ICW1,MASTER_8259_PORT);
+    outb(ICW2_MASTER, MASTER_8259_DATA);
+    outb(ICW3_MASTER,MASTER_8259_DATA);
+    outb(ICW4,MASTER_8259_DATA);
+    // Second comes to SLAVE pic
+    outb(ICW1,SLAVE_8259_PORT);
+    outb(ICW2_SLAVE,SLAVE_8259_DATA);
+    outb(ICW3_SLAVE,SLAVE_8259_DATA);
+    outb(ICW4,SLAVE_8259_DATA);
 
-    outb_p(ICW1,SLAVE_8259_PORT);
-    outb_p(MASTER_8259_PORT + 8,SLAVE_8259_PORT +1);
-    outb_p(ICW3_SLAVE,0xA1);
-    outb_p(ICW4,0xA1);
-    if (eoi){
-        disable_irq(   );
-    }else{
-        // mask and ack PIC
-    }
-    outb(cached 21, 0x21);
-    outb(cached A1, 0xA1);
     
-    // spin_unlock_irqstore(&i8259_lock,flags);
+    outb(master_mask, MASTER_8259_DATA);
+    outb(slave_mask, SLAVE_8259_DATA);
+    
+
+    enable_irq( ICW3_SLAVE );
+    sti() // why we should call it here
+    restore_flags(flags);
 }
 
-/* Enable (unmask) the specified IRQ */
+
+/* 
+ * enable_irq
+ *  DESCRIPTION: enable the specific port on i8259,
+ *       accept the interrup specified by irq_num
+ *  INPUTS: irq_num -- interrupt request id, 0 - 15
+ *  OUTPUTS: none
+ *  RETURN VALUE: none
+ *  SIDE EFFECTS: Enable (unmask) the specified IRQ
+ */
 // about 6
 void enable_irq(uint32_t irq_num) {
-}
+    if (irq_num < 0 || irq_num > 15){   
+        printf("Failed to find corrsponding port");
+        return ;
+    }
+    if (irq_num <8)
+    {// if it is master 
+        master_mask &= ~( 1 << irq_num);
+        outb(master_mask,MASTER_8259_DATA);
 
-/* Disable (mask) the specified IRQ */
+    }else{// then it's slave
+        slave_mask &= ~(1 << (irq_num-8));
+        outb(slave_mask,SLAVE_8259_DATA);
+
+    }
+    
+    // TODO : correct value
+}
+/* 
+ * disable_irq
+ *  DESCRIPTION: disable the specific port on i8259,
+ *  to stop accepting the interrup specified by irq_num
+ *  INPUTS: irq_num -- interrupt request id, 0 - 15
+ *  OUTPUTS: none
+ *  RETURN VALUE: none
+ *  SIDE EFFECTS: Disable (mask) the specified IRQ
+ */
+
 // about 6 
 void disable_irq(uint32_t irq_num) {
-}
+    if (irq_num < 0 || irq_num > 15){   
+        printf("Failed to find corrsponding port");
+        return;
+    }
+    if (irq_num <8)
+    {// if it is master 
+        master_mask |= ~(1 << irq_num);
+        outb(master_mask,MASTER_8259_DATA);
 
-/* Send end-of-interrupt signal for the specified IRQ */
+    }else{// then it's slave
+        slave_mask |= ~(1 << (irq_num-8));
+        outb(slave_mask,SLAVE_8259_DATA);
+
+    }
+}
+/* 
+ * send_eoi
+ *  DESCRIPTION: OR EOI signal with the port on i8259,
+ *  specified by irq_num
+ *  INPUTS: irq_num -- interrupt request id, 0 - 15
+ *  OUTPUTS: none
+ *  RETURN VALUE: none
+ *  SIDE EFFECTS: Send end-of-interrupt signal for the specified IRQ
+ */
 // about 5
 void send_eoi(uint32_t irq_num) {
+    if (irq_num < 0 || irq_num > 15){   
+        printf("Failed to find corrsponding port");
+        return ;
+    }
+    if (irq_num <8)
+    {// if it is master 
+        master_mask |= EOI;
+        outb(master_mask,MASTER_8259_PORT);
+
+    }else{// then it's slave
+        slave_mask |= EOI;
+        outb(slave_mask,SLAVE_8259_PORT);
+        // TODO: OR WTIH 2?
+
+    }
 }
