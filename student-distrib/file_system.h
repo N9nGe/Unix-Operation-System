c8b4ef3cfe88af76dfa5edac7f6d4dda6767b2ce
yiming14 <yiming14@illinois.edu>
2022-10-19 21:40:43 -0500

    Add functions to file system

--- student-distrib/file_system.h
+++ student-distrib/file_system.h
@@ -2,3 +2,21 @@
 #include "lib.h"
 #include "types.h"
 
+void file_system_init();
+
+int file_open();
+
+void file_read();
+
+int file_write();
+
+int file_close();
+
+int dir_open();
+
+void dir_read();
+
+int dir_write();
+
+int dir_close();
+
