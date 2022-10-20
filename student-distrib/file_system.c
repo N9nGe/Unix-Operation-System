cb0c25059a3e546e59a283c957f2d4b49bdf1af1
yiming14 <yiming14@illinois.edu>
2022-10-19 22:23:33 -0500

    Add changes to the file system

--- student-distrib/file_system.c
+++ student-distrib/file_system.c
@@ -1,1 +1,58 @@
 #include "file_system.h"
+
+data_block_t * data_block_ptr;
+
+inode_t * inode_ptr;
+
+dentry_t * dentry_ptr;
+
+boot_block_t * boot_block_ptr;
+
+void file_system_init(uint32_t* fs_start) {
+    
+}
+
+int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry){
+
+}
+
+int32_t read_dentry_by_index(const uint8_t* fname, dentry_t* dentry){
+
+}
+
+int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length){
+    
+}
+
+int file_open() {
+    return 0;
+}
+
+void file_read() {
+
+}
+
+int file_write() {
+    return -1;
+}
+
+int file_close() {
+    return 0;
+}
+
+int dir_open() {
+    return 0;
+}
+
+void dir_read(int32_t fd, void* buf, int32_t nbytes) {
+
+}
+
+int dir_write() {
+    return -1;
+}
+
+int dir_close() {
+    return 0;
+}
+
--- student-distrib/file_system.h
+++ student-distrib/file_system.h
@@ -2,7 +2,31 @@
 #include "lib.h"
 #include "types.h"
 
-void file_system_init();
+typedef struct data_block {
+    uint32_t random;
+} data_block_t;
+
+typedef struct inode {
+    uint32_t random;
+} inode_t;
+
+typedef struct dentry {
+    uint32_t random;
+} dentry_t;
+
+typedef struct boot_block {
+    uint32_t dentry_number;
+    uint32_t inode_number;
+    uint32_t data_blocks;
+} boot_block_t;
+
+void file_system_init(uint32_t* fs_start);
+
+int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
+
+int32_t read_dentry_by_index(const uint8_t* fname, dentry_t* dentry);
+
+int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
 
 int file_open();
 
@@ -14,7 +38,7 @@ int file_close();
 
 int dir_open();
 
-void dir_read();
+void dir_read(int32_t fd, void* buf, int32_t nbytes);
 
 int dir_write();
 
