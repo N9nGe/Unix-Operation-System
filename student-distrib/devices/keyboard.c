4991d231d3c99d47b49e48df539b0fdb082dd4bf
Yiming Zhao <yiming14@illinois.edu>
2022-11-07 15:30:23 -0600

    Changed keyboard for other tests

--- student-distrib/devices/keyboard.c
+++ student-distrib/devices/keyboard.c
@@ -155,7 +155,11 @@ void keyboard_interrupt_handler(){
                 if ( value == '\n' ){
                     // memset(keyboard_buf,NULL,sizeof(keyboard_buf)); // TODO: MOVE TO TERMINA
                     terminal_count = keybuf_count +1;
-                    if(terminal_count == KEY_BUF_SIZE - 1) keyboard_buf[keybuf_count+1] = '\n';
+                    if(terminal_count == KEY_BUF_SIZE - 1) {
+                        keyboard_buf[keybuf_count+1] = '\n';
+                    } else {
+                        keyboard_buf[keybuf_count] = '\n';
+                    }
                     keybuf_count++;
                     keybuf_count = 0;
                     putc_advanced(value);
