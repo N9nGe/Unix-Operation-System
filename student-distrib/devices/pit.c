e4c5f7c9486f68db70af415ae6efcd19301b4582
ngao4 <ngao4@illinois.edu>
2022-11-27 15:52:53 -0600

    finish pit initialize but the handler need to be added for the schduler stuff

--- student-distrib/devices/pit.c
+++ student-distrib/devices/pit.c
@@ -1,19 +1,6 @@
 #include"pit.h"
 #include"../i8259.h"
 
-/* Programmable Interval Timer Driver
- * Author: Tony Wang -- 1 11/18
- * 
-*/
-#define PIT_IRQ_NUM 	0x00
-#define PIT_SUCCESS 	0
-#define PIT_FAIL		-1
-//only need channel one, other 2 are useless
-#define PIT_DATA_PORT	0x40
-#define PIT_MODE_PORT	0x43
-/* Mode: Channel 0, mode 3 */
-#define PIT_MODE_2        0x36 	// Square wave, to seperate the mode evenly with 3 terminals      
-#define PIT_FREQ_SET	0x10000 // Slowest freq, but how to judge?      
 /*
 The PIT chip uses the following I/O ports:
 
@@ -30,61 +17,22 @@ void pit_init(){
     uint32_t flags;
     cli_and_save(flags);    // CLI and save EFLAGS
 
-	outb(PIT_MODE_2,PIT_MODE_PORT);
+	// set the squared wave mode
+	outb(PIT_MODE_3, PIT_MODE_PORT);
 
-	outb(PIT_FREQ_SET,PIT_DATA_PORT);
+	// set the devisor frequency(16 bit) 
+	outb(PIT_OSCILLATOR_FREQ & PIT_LOW_MASK, PIT_DATA_PORT); // get the low 8 bit of the frequency
+	outb(PIT_OSCILLATOR_FREQ >> 8, PIT_DATA_PORT);  // get the high 8 bit of the frequency
 
     sti(); // set interrupt flags
     restore_flags(flags);   // restore flags 
-
     // generate the interrupt by sending the signal to the PIC
     enable_irq(PIT_IRQ_NUM);
-    return;
 }
 
 void pit_interrupt_handler(){
     cli();
     send_eoi(PIT_IRQ_NUM);
-	// printf("hello, pit! ");
-	// Used for sheduling
+	//TODO: NEED TO add the multiterminal stuff to do the context switch(flag. global process...)
     sti();
-}
-/*
-IRQ0_handler:
-	push eax
-	push ebx
- 
-	mov eax, [IRQ0_fractions]
-	mov ebx, [IRQ0_ms]                    ; eax.ebx = amount of time between IRQs
-	add [system_timer_fractions], eax     ; Update system timer tick fractions
-	adc [system_timer_ms], ebx            ; Update system timer tick milli-seconds
- 
-	mov al, 0x20
-	out 0x20, al                          ; Send the EOI to the PIC
- 
-	pop ebx
-	pop eax
-	iretd
-*/
-
-
-
-/*	PIT's related syscall
- *	Useless for now, but leave the IO here for further development
- *	-1 -- fail, default for unrealized functionality
- *
- * 
-
-*/
-int32_t pit_open(const uint8_t * filename){
-	return -1;
-}
-int32_t pit_read(int32_t fd, void* buf, int32_t nbytes){
-	return -1;
-}
-int32_t pit_write(int32_t fd, const void* buf, int32_t nbytes){
-	return -1;
-}
-int32_t pit_close(int32_t fd){
-	return -1;
 }
--- student-distrib/devices/pit.h
+++ student-distrib/devices/pit.h
@@ -4,19 +4,20 @@
 #include"../lib.h"
 #include"../types.h"
 
-
+#define PIT_IRQ_NUM 		  0x00
+#define PIT_SUCCESS 		  0
+#define PIT_FAIL			  -1
+//only need channel one, other 2 are useless
+#define PIT_DATA_PORT		  0x40
+#define PIT_MODE_PORT		  0x43
+/* Mode: Channel 0, mode 3 */
+#define PIT_MODE_3      	  0x36 	// Square wave, to seperate the mode evenly with 3 terminals      
+#define PIT_FREQ			  100 	// Slowest freq, but how to judge? 
+#define PIT_OSCILLATOR_FREQ   (1193180 / PIT_FREQ) + 1  
+#define PIT_LOW_MASK          0xFF  
 
 void pit_init();
 
 void pit_interrupt_handler();
-
-
-
-
-
-int32_t pit_open(const uint8_t * filename);
-int32_t pit_read(int32_t fd, void* buf, int32_t nbytes);
-int32_t pit_write(int32_t fd, const void* buf, int32_t nbytes);
-int32_t pit_close(int32_t fd);
 
 #endif /*_PIT_H_*/
