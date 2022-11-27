c55ec121f718f0c77b01fc5ba2e9dc9dfe615fac
everloom <jiew5@illinois.edu>
2022-11-27 15:38:46 -0600

    merge scheduler mannually

---
+++ student-distrib/scheduling.h
@@ -0,0 +1,15 @@
+#ifndef SCHEDULING_H
+#define SCHEDULING_H
+
+#include "lib.h"
+#include "paging.h"
+#include "syscall.h"
+#include "types.h"
+#include "devices/keyboard.h"
+#include "devices/terminal.h"
+
+void scheduler();
+
+
+#endif /* SCHEDULING_H */
+
